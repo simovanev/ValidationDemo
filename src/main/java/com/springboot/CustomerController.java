50d2f23484f56441571abfc29f51947c59bb0f00
Simovanev <simovanev@abv.bg>
2024-09-12 05:01:11 +0300

    @PostMapping with @Valid

--- src/main/java/com/springboot/CustomerController.java
+++ src/main/java/com/springboot/CustomerController.java
@@ -1,8 +1,12 @@
 package com.springboot;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -10,5 +14,12 @@ public class CustomerController {
     public String showForm(Model model){
         model.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer
+            , BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "customer-form";
+        }else return "customer-confitrmation";
     }
 }
