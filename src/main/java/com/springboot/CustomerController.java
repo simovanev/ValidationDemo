0abfc71c35a1f6deb21655351bc5ffc33fec668b
Simovanev <simovanev@abv.bg>
2024-09-12 05:50:24 +0300

    Add @InitBinder and StringTrimmerEditor to trim the whitespaces.

--- src/main/java/com/springboot/CustomerController.java
+++ src/main/java/com/springboot/CustomerController.java
@@ -1,15 +1,23 @@
 package com.springboot;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
     @GetMapping("/")
     public String showForm(Model model){
         model.addAttribute("customer", new Customer());
@@ -18,6 +26,7 @@ public class CustomerController {
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer customer
             , BindingResult bindingResult){
+        System.out.println("Last Name: "+"|"+customer.getLastName()+"|");
         if (bindingResult.hasErrors()) {
             return "customer-form";
         }else return "customer-confirmation";
